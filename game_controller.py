251c72e5f3e4bf9839493bac6eb42dd71f8eb64b
Jędrzej Jamnicki <jedrzej.jamnicki@gmail.com>
2024-04-03 17:56:14 +0200

    vm

--- game_controller.py
+++ game_controller.py
@@ -6,7 +6,7 @@ from typing import Generator, Optional, Tuple
 
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR/tesseract.exe"
 
 import cv2
 import numpy as np
@@ -36,7 +36,8 @@ from vision_detector import VisionDetector
 def scale_img(img, scale=5):
     width = int(img.shape[1] * scale)
     height = int(img.shape[0] * scale)
-    return cv2.resize(img, (width, height), interpolation = cv2.INTER_AREA)
+    return cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
+
 
 def binarized_to_grayscale(binarized_img):
     # return np.array([[[x, x, x] for x in row] for row in binarized_img], dtype=np.uint8) * 255
@@ -47,6 +48,7 @@ def binarized_to_grayscale(binarized_img):
     grayscale_img = np.expand_dims(binarized_img, axis=-1) * 255
     # Stack the single channel image across the three channels (RGB)
     return np.repeat(grayscale_img, 3, axis=-1)
+
 
 def get_user_pos_from_minimap_text(text):
     pattern = re.compile(r"(\d+),\s*(\d+)")
@@ -59,7 +61,12 @@ def get_user_pos_from_minimap_text(text):
 
 class GameController:
 
-    def __init__(self, vision_detector: VisionDetector, start_delay: float = 5, saved_credentials_idx: int = 1):
+    def __init__(
+        self,
+        vision_detector: VisionDetector,
+        start_delay: float = 5,
+        saved_credentials_idx: int = 1,
+    ):
         self.vision_detector = vision_detector
         self.start_delay = start_delay
 
@@ -133,8 +140,10 @@ class GameController:
         self._check_key_value(key.value)
         self.keyboard.release(key.value)
         logger.trace(f"Key `{key}` released")
-    
-    def click(self, right: bool = False, times: int = 1, press_time: Optional[float] = None):
+
+    def click(
+        self, right: bool = False, times: int = 1, press_time: Optional[float] = None
+    ):
         for _ in range(times):
             self._click(right, press_time)
 
@@ -158,17 +167,19 @@ class GameController:
     def _release_mouse_btn(self, right=False):
         self.mouse.release(Button.right if right else Button.left)
 
-    def click_at(self, pos: Tuple[int, int], right=False, times: int = 1):
+    def click_at(self, pos: Tuple[int, int], right=False, times: int = 1, fast=False):
         self.move_cursor_at(pos)
-        sleep(0.5)
+        sleep(0.5 if not fast else 0.1)
         self.click(right=right, times=times)
 
-    def get_user_position(self, vis_detector: VisionDetector, frame: np.array, indicator_pos) -> Tuple[np.array, Tuple[int, int]]:
+    def get_user_position(
+        self, vis_detector: VisionDetector, frame: np.array, indicator_pos
+    ) -> Tuple[np.array, Tuple[int, int]]:
         # minimap_user_pos = (WINDOW_WIDTH - 62, 100)
         minimap_user_pos = indicator_pos
         rgb_text_main_color = (255, 215, 76)
         bbox_thickness = 2
-        ocr_config = '--psm 7 --oem 1'
+        ocr_config = "--psm 7 --oem 1"
 
         self.move_cursor_at(minimap_user_pos)
         # w, h = 120, 20
@@ -178,22 +189,36 @@ class GameController:
         frame = VisionDetector.mark_bbox(frame, x, y, w, h, thickness=bbox_thickness)
 
         frame_after_mouse_move = vis_detector.capture_frame()
-        cropped_user_pos_text_img = VisionDetector.crop_bbox(frame_after_mouse_move, x, y, w, h)
+        cropped_user_pos_text_img = VisionDetector.crop_bbox(
+            frame_after_mouse_move, x, y, w, h
+        )
         logger.warning(f"\n\n{cropped_user_pos_text_img.shape=}")
-        cropped_user_pos_text_img = cv2.cvtColor(cropped_user_pos_text_img, cv2.COLOR_BGR2RGB)
+        cropped_user_pos_text_img = cv2.cvtColor(
+            cropped_user_pos_text_img, cv2.COLOR_BGR2RGB
+        )
 
-        erode_kernel = np.ones((3,3), np.uint8)
+        erode_kernel = np.ones((3, 3), np.uint8)
         upscaled_img = scale_img(cropped_user_pos_text_img, scale=5)
-        upscaled_binarized_img = np.where(np.all(upscaled_img == rgb_text_main_color, axis=-1), 0, 1)
-        upscaled_binarized_img_grayscale = binarized_to_grayscale(upscaled_binarized_img)
-        eroded_upscaled_binarized_img = cv2.erode(upscaled_binarized_img_grayscale, erode_kernel, iterations = 1)
-        minimap_user_text = pytesseract.image_to_string(eroded_upscaled_binarized_img, config=ocr_config)
+        upscaled_binarized_img = np.where(
+            np.all(upscaled_img == rgb_text_main_color, axis=-1), 0, 1
+        )
+        upscaled_binarized_img_grayscale = binarized_to_grayscale(
+            upscaled_binarized_img
+        )
+        eroded_upscaled_binarized_img = cv2.erode(
+            upscaled_binarized_img_grayscale, erode_kernel, iterations=1
+        )
+        minimap_user_text = pytesseract.image_to_string(
+            eroded_upscaled_binarized_img, config=ocr_config
+        )
         user_position = get_user_pos_from_minimap_text(minimap_user_text)
 
         logger.info(f"User position: {user_position}")
         return frame, user_position
 
-    def move_cursor_at(self, pos: Tuple[int, int], after_move_wait: Optional[float] = None):
+    def move_cursor_at(
+        self, pos: Tuple[int, int], after_move_wait: Optional[float] = None
+    ):
         self.mouse.position = pos
         if after_move_wait is None:
             self._after_mouse_move_wait()
@@ -245,14 +270,14 @@ class GameController:
 
     def _mouse_btn_release_wait(self):
         sleep(self._release_mouse_btn_delay())
-    
+
     @staticmethod
     def _after_mouse_move_delay():
         return uniform(0.2, 0.25)
 
     def _after_mouse_move_wait(self):
         sleep(self._after_mouse_move_delay())
-    
+
     @staticmethod
     def _start_delay(delay):
         logger.info(f"Starting in {delay} seconds...")
@@ -273,7 +298,7 @@ class GameController:
         logger.info("Toggling minimap visibility...")
         self.press_with(GameBind.SHIFT_MINIMAP, Key.shift_l)
         self.minimap_visible = not self.minimap_visible
-    
+
     def hide_minimap(self):
         if self.minimap_visible:
             self.toggle_minimap()
@@ -283,7 +308,9 @@ class GameController:
             self.toggle_minimap()
 
     def turn_randomly(self, turn_press_time: float = 0.6):
-        possible_turns = list(set([Key.up, Key.down, Key.left, Key.right]) - {self.last_turn})
+        possible_turns = list(
+            set([Key.up, Key.down, Key.left, Key.right]) - {self.last_turn}
+        )
         turn = choice(possible_turns)
         self.tap_key(turn, press_time=turn_press_time)
         self.last_turn = turn
@@ -292,7 +319,9 @@ class GameController:
         logger.info("Using boosters...")
         self.tap_key(GameBind.BOOSTERS)
 
-    def toggle_skill(self, skill_key: str, reset_animation: bool = True, animation_delay: float = 1):
+    def toggle_skill(
+        self, skill_key: str, reset_animation: bool = True, animation_delay: float = 1
+    ):
         if self.mounted:
             logger.warning("Cannot toggle skills while mounted!")
             return
@@ -306,7 +335,7 @@ class GameController:
     def toggle_passive_skills(self, reset_animation: bool = True):
         self.toggle_skill(UserBind.BERSERK, reset_animation=reset_animation)
         self.toggle_skill(UserBind.AURA, reset_animation=reset_animation)
-    
+
     def start_attack(self):
         if not self.attacking:
             self.press_key(GameBind.ATTACK)
@@ -320,12 +349,13 @@ class GameController:
     @staticmethod
     def _check_key_value(key):
         if isinstance(key, str) and key.isupper():
-            logger.warning(f"Key `{key}` is uppercase! It should be lowercase to be interpreted correctly.")
+            logger.warning(
+                f"Key `{key}` is uppercase! It should be lowercase to be interpreted correctly."
+            )
 
     def _init_keyboard_listener(self):
         keyboard_listener = KeyboardListener(
-            on_press=self._on_press,
-            on_release=self._on_release
+            on_press=self._on_press, on_release=self._on_release
         )
         return keyboard_listener
 
@@ -337,7 +367,9 @@ class GameController:
 
     def _on_click(self, x, y, button, pressed):
         frame_x, frame_y = self.vision_detector.get_frame_pos((x, y))
-        logger.trace(f"Mouse clicked at global: ({x}, {y}) frame: ({frame_x}, {frame_y}) with button `{button}`")
+        logger.trace(
+            f"Mouse clicked at global: ({x}, {y}) frame: ({frame_x}, {frame_y}) with button `{button}`"
+        )
 
     def _on_press(self, key):
         logger.trace(f"Key `{key}` pressed")
@@ -346,7 +378,7 @@ class GameController:
 
     def _on_release(self, key):
         logger.trace(f"Key `{key}` released")
-    
+
     def exit(self):
         logger.info("Exiting...")
         self.stop_attack()
@@ -384,17 +416,31 @@ class GameController:
 
     def calibrate_game_settings(self):
         self.show_settings()
-        
-        self.click_at(self.vision_detector.get_global_pos(positions.GAME_SYS_SETTINGS_BTN_CENTER))
-        self.click_at(self.vision_detector.get_global_pos(positions.GSS_CAM_CLOSER_BTN_CENTER))
-        self.click_at(self.vision_detector.get_global_pos(positions.GSS_FOG_LIGHT_BTN_CENTER))
+
+        self.click_at(
+            self.vision_detector.get_global_pos(positions.GAME_SYS_SETTINGS_BTN_CENTER)
+        )
+        self.click_at(
+            self.vision_detector.get_global_pos(positions.GSS_CAM_CLOSER_BTN_CENTER)
+        )
+        self.click_at(
+            self.vision_detector.get_global_pos(positions.GSS_FOG_LIGHT_BTN_CENTER)
+        )
 
         self.hide_settings()
         self.show_settings()
 
-        self.click_at(self.vision_detector.get_global_pos(positions.GAME_GAME_SETTINGS_BTN_CENTER))
-        self.click_at(self.vision_detector.get_global_pos(positions.GGS_HIDE_CHAT_BTN_CENTER))
-        self.click_at(self.vision_detector.get_global_pos(positions.GGS_NAME_ITEMS_ONLY_BTN_CENTER))
+        self.click_at(
+            self.vision_detector.get_global_pos(positions.GAME_GAME_SETTINGS_BTN_CENTER)
+        )
+        self.click_at(
+            self.vision_detector.get_global_pos(positions.GGS_HIDE_CHAT_BTN_CENTER)
+        )
+        self.click_at(
+            self.vision_detector.get_global_pos(
+                positions.GGS_NAME_ITEMS_ONLY_BTN_CENTER
+            )
+        )
 
     def toggle_settings(self):
         self.tap_key(GameBind.SETTINGS)
@@ -417,7 +463,7 @@ class GameController:
 
     def move_camera_right(self, press_time: float):
         self.tap_key(GameBind.CAMERA_RIGHT, press_time=press_time)
-    
+
     def move_camera_down(self, press_time: float):
         self.tap_key(GameBind.CAMERA_DOWN, press_time=press_time)
 
@@ -428,7 +474,14 @@ class GameController:
         self.tap_key(GameBind.CAMERA_ZOOM_OUT, press_time=press_time)
 
     def steer_randomly(self, press_time: float = 2):
-        steer_action = choice([self.steer_up_right, self.steer_up_left, self.steer_down_right, self.steer_down_left])
+        steer_action = choice(
+            [
+                self.steer_up_right,
+                self.steer_up_left,
+                self.steer_down_right,
+                self.steer_down_left,
+            ]
+        )
         steer_action(press_time=press_time)
 
     def steer_up_right(self, press_time: float = 2):
@@ -463,7 +516,7 @@ class GameController:
         if not self.eq_visible:
             self.tap_key(GameBind.EQ_MENU)
             self.eq_visible = True
-    
+
     def hide_eq(self):
         if self.eq_visible:
             self.tap_key(GameBind.EQ_MENU)
@@ -475,7 +528,9 @@ class GameController:
         if slot not in range(1, 5):
             logger.warning(f"Invalid slot number: {slot}")
             return
-        slot_global_center = self.vision_detector.get_global_pos(positions.EQ_SLOT_SELECT_BTNS[slot - 1])
+        slot_global_center = self.vision_detector.get_global_pos(
+            positions.EQ_SLOT_SELECT_BTNS[slot - 1]
+        )
         self.click_at(slot_global_center)
 
     def move_full_butelka_dywizji(self):
@@ -487,19 +542,25 @@ class GameController:
             logger.warning("No bottles found!. Cannot move the full butelka dywizji!")
             return
         top_left_bottle_loc = bottles_locs[0]
-        top_left_bottle_global_loc = self.vision_detector.get_global_pos(top_left_bottle_loc)
+        top_left_bottle_global_loc = self.vision_detector.get_global_pos(
+            top_left_bottle_loc
+        )
         self.grab_item(top_left_bottle_global_loc)
         self.show_eq_slot(4)
         empty_slots_locs = self.vision_detector.detect_empty_items_slots(frame)
         if len(empty_slots_locs) == 0:
-            logger.warning("No empty slots found!. Cannot move the full butelka dywizji!")
+            logger.warning(
+                "No empty slots found!. Cannot move the full butelka dywizji!"
+            )
             self.click(right=True)  # cancel grabbing
             self.hide_eq()
             return
 
         empty_slot_loc = empty_slots_locs[0]
         empty_slot_global_loc = self.vision_detector.get_global_pos(empty_slot_loc)
-        logger.info(f"Moving the full butelka dywizji to the empty slot (location on the screen: {empty_slot_global_loc})...")
+        logger.info(
+            f"Moving the full butelka dywizji to the empty slot (location on the screen: {empty_slot_global_loc})..."
+        )
         self.put_item(empty_slot_global_loc)
         self.hide_eq()
 
@@ -512,11 +573,19 @@ class GameController:
             logger.warning("No empty bottles found!")
             return
         top_left_bottle_loc = bottles_locs[0]
-        top_left_bottle_global_loc = self.vision_detector.get_global_pos(top_left_bottle_loc)
-        logger.info(f"Using the next butelka dywizji (location on the screen: {top_left_bottle_global_loc})...")
-        self.click_at(top_left_bottle_global_loc, right=True)  # start filling the next bottle
+        top_left_bottle_global_loc = self.vision_detector.get_global_pos(
+            top_left_bottle_loc
+        )
+        logger.info(
+            f"Using the next butelka dywizji (location on the screen: {top_left_bottle_global_loc})..."
+        )
+        self.click_at(
+            top_left_bottle_global_loc, right=True
+        )  # start filling the next bottle
         # confirmation_btn_center = (360, 320)
-        confirmation_btn_center_global = self.vision_detector.get_global_pos(positions.UZYJ_BUTELKE_CONFIRMATION_BTN)
+        confirmation_btn_center_global = self.vision_detector.get_global_pos(
+            positions.UZYJ_BUTELKE_CONFIRMATION_BTN
+        )
         self.click_at(confirmation_btn_center_global)  # confirm filling the bottle
         self.hide_eq()
 
@@ -526,13 +595,17 @@ class GameController:
         sleep(0.4)
 
     def put_item(self, slot_pos: Tuple[int, int]):
-        logger.debug(f"Putting the item to the slot (location on the screen: {slot_pos})...")
+        logger.debug(
+            f"Putting the item to the slot (location on the screen: {slot_pos})..."
+        )
         self.click_at(slot_pos)
         sleep(0.4)
 
     def login(self):
         # login_btn_center = (400, 300)
-        login_btn_global_center = self.vision_detector.get_global_pos(positions.LOGIN_BTN_CENTER)
+        login_btn_global_center = self.vision_detector.get_global_pos(
+            positions.LOGIN_BTN_CENTER
+        )
         self.load_saved_credentials(idx=self.saved_credentials_idx)
         self.click_at(login_btn_global_center)
         sleep(10)  # wait for character select menu to load
@@ -546,16 +619,19 @@ class GameController:
         logger.info(f"Loading the credentials... ({idx})")
         cred_btn_center = (
             positions.LOAD_CREDENTIAL_BTN_CENTER[0],
-            positions.LOAD_CREDENTIAL_BTN_CENTER[1] + idx * positions.LOAD_CREDENTIAL_BTN_SPACING
+            positions.LOAD_CREDENTIAL_BTN_CENTER[1]
+            + idx * positions.LOAD_CREDENTIAL_BTN_SPACING,
         )
         cred_btn_global_center = self.vision_detector.get_global_pos(cred_btn_center)
         self.click_at(cred_btn_global_center)
 
     def open_game(self, load_wait: float = 30):
         logger.info("Opening the game...")
-        game_dir = os.path.dirname(self.game_exe_path)  # Assumes the game's working directory is its location.
+        game_dir = os.path.dirname(
+            self.game_exe_path
+        )  # Assumes the game's working directory is its location.
         # Set the working directory to the game's directory and run as admin.
-        command = fr'Powershell -Command "&{{Set-Location -Path {game_dir}; Start-Process "{self.game_exe_path}" -Verb RunAs}}"'
+        command = rf'Powershell -Command "&{{Set-Location -Path {game_dir}; Start-Process "{self.game_exe_path}" -Verb RunAs}}"'
         os.system(command)
         logger.debug(f"Waiting for the game to load... ({load_wait}s)")
         sleep(load_wait)  # wait for the game to load
@@ -569,7 +645,7 @@ class GameController:
         self.tap_key(GameBind.MAP, press_time=1)
         self._key_release_wait()  # to prevent pressing other keys too fast
         self.map_visible = not self.map_visible
-    
+
     def hide_map(self):
         logger.info("Hiding the map...")
         if self.map_visible:
@@ -583,7 +659,9 @@ class GameController:
     def teleport_to_polana(self, after_tp_wait: float = 10):
         logger.info("Teleporting to Leśna Polana...")
         self.show_map()
-        map_polana_btn_global_center = self.vision_detector.get_global_pos(positions.MAP_POLANA_BTN_CENTER)
+        map_polana_btn_global_center = self.vision_detector.get_global_pos(
+            positions.MAP_POLANA_BTN_CENTER
+        )
         self.click_at(map_polana_btn_global_center)
         logger.debug(f"Waiting for the teleportation to finish... ({after_tp_wait}s)")
         sleep(after_tp_wait)  # wait for the teleportation to finish
@@ -591,14 +669,16 @@ class GameController:
         # because the its hidden after the teleportation
         self.reset_game_visibility_state()
 
-    def idle(self, time: float,
-             capture: bool = False,
-             use_boosters: bool = True,
-             turn_randomly: bool = False,
-             pickup: bool = False,
-             lure: bool = False,
-             act_seq_wait: Optional[float] = None
-    # ) -> Generator[None | np.ndarray, None, None]:
+    def idle(
+        self,
+        time: float,
+        capture: bool = False,
+        use_boosters: bool = True,
+        turn_randomly: bool = False,
+        pickup: bool = False,
+        lure: bool = False,
+        act_seq_wait: Optional[float] = None,
+        # ) -> Generator[None | np.ndarray, None, None]:
     ) -> None:
         # yield_frame = None
         t0 = perf_counter()
